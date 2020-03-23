429ade1b3348bbab93b3d2b14dc6a3af08be06f8
Signoraelle <alicecasadei@icloud.com>
2020-03-24 00:20:03 +0100

    up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
 import "./App.css";
 
 class App extends Component {
   render() {
-    const name = "John Doe";
     return (
       <div className="App">
-        <h1> Hello {name} </h1>
+        <Navbar />
+        <Users />
       </div>
     );
   }
